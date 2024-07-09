4127676d2ab7cdbffc2288c2568b386393329d2a
Shahid Uddin Ahmed <shahidshakib0@gmail.com>
2024-07-09 18:01:33 +0600

    tb of reg_PISO and uart_top 	modified:   tb/reg_piso_tb.sv 	new file:   tb/uart_top_tb.sv

--- tb/uart_top_tb.sv
+++ tb/uart_top_tb.sv
@@ -1,50 +1,60 @@
 /*
-Testbench of PISO register
+Testbench of UART TOP  
 Author : Shahid Uddin Ahmed (shahidshakib0@gmail.com)
 */
 
-module reg_piso_tb;
+module uart_top_tb;
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-SIGNALS
   //////////////////////////////////////////////////////////////////////////////////////////////////
   logic arst_n; // active low asynchronous reset
   logic clk; // global clock signal
-  logic [2:0] d_reg_sel; // 3-bit select signal to choose which bit to output
-  logic d_reg_en; // enable signal for register
   logic [7:0] data; // 8-bit input data
-  logic data_out; // single bit output data
+  logic [1:0] num_data; // number of data bits
+  logic parity; // parity enable signal
+  logic stop_2; // 2 stop bits enable signal
+  logic valid; // valid signal for UART transmission
+  logic ready; // ready signal from UART
+  logic Tx; // UART transmit signal
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-VARIABLES
   //////////////////////////////////////////////////////////////////////////////////////////////////
-  int pass;  // number of times results matched
-  int fail;  // number of times results did not match
-  logic [7:0] u_register; // internal 8-bit register to store data
+  int         pass;  // number of times results matched
+  int         fail;  // number of times results did not match
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-RTLS
+  //-RTLS 
   //////////////////////////////////////////////////////////////////////////////////////////////////
-  reg_piso u_reg_piso (
+  uart_top u_uart_top (
     .clk(clk),
     .arst_n(arst_n),
-    .d_reg_en(d_reg_en),
-    .d_reg_sel(d_reg_sel),
     .data(data),
-    .data_out(data_out)
+    .num_data(num_data),
+    .parity(parity),
+    .stop_2(stop_2),
+    .valid(valid),
+    .ready(ready),
+    .Tx(Tx)
   );
 
-  // Driver Mailbox for Inputs
+  // Driver Mailboxes for Inputs
   mailbox #(logic [7:0]) data_dvr_mbx  = new();
-  mailbox #(logic) d_reg_en_dvr_mbx  = new();
-  mailbox #(logic [2:0]) d_reg_sel_dvr_mbx  = new();
+  mailbox #(logic [1:0]) num_data_dvr_mbx  = new();
+  mailbox #(logic) parity_dvr_mbx  = new();
+  mailbox #(logic) stop_2_dvr_mbx  = new();
+  mailbox #(logic) valid_dvr_mbx  = new();
 
-  // Monitor Mailbox For Inputs and Outputs
+  // Monitor Mailboxes for Inputs and Outputs
   mailbox #(logic [7:0]) data_mon_mbx  = new();
-  mailbox #(logic) d_reg_en_mon_mbx  = new();
-  mailbox #(logic [2:0]) d_reg_sel_mon_mbx  = new();
-  mailbox #(logic) data_out_mon_mbx  = new();
-
+  mailbox #(logic [1:0]) num_data_mon_mbx  = new();
+  mailbox #(logic) parity_mon_mbx  = new();
+  mailbox #(logic) stop_2_mon_mbx  = new();
+  mailbox #(logic) valid_mon_mbx  = new();
+  mailbox #(logic) ready_mon_mbx  = new();
+  mailbox #(logic) Tx_mon_mbx  = new();
+  
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-METHODS
   //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -54,17 +64,18 @@ module reg_piso_tb;
     #100ns;
     clk        <= '0;
     arst_n     <= '0;
-    d_reg_sel  <= '0;
-    d_reg_en   <= '0;
     data       <= '0;
-    u_register <= '0;
+    num_data   <= '0;
+    parity     <= '0;
+    stop_2     <= '0;
+    valid      <= '0;
     #100ns;
     arst_n     <= '1;
     #100ns;
   endtask
 
   // Start toggling system clock forever every 5ns
-  task static start_clk_i();
+  task static start_clk();
     fork
       forever begin
         clk <= '1;
@@ -72,75 +83,65 @@ module reg_piso_tb;
         clk <= '0;
         #5ns;
       end
-    join_none
+   join_none
   endtask
 
   // Driver, monitor and scoreboard representation
   task static driver_monitor_scoreboard();
     fork
       forever begin // in driver
-
         logic [7:0] tem_data;
-        logic       tem_d_red_en;
-        logic [2:0] tem_d_reg_sel;
-
+        logic [1:0] tem_num_data;
+        logic       tem_parity;
+        logic       tem_stop_2;
+        logic       tem_valid;
+        
         data_dvr_mbx.get(tem_data);
-        d_reg_en_dvr_mbx.get(tem_d_red_en);
-        d_reg_sel_dvr_mbx.get(tem_d_reg_sel);
+        num_data_dvr_mbx.get(tem_num_data);
+        parity_dvr_mbx.get(tem_parity);
+        stop_2_dvr_mbx.get(tem_stop_2);
+        valid_dvr_mbx.get(tem_valid);
 
         data <= tem_data;
-        d_reg_en <= tem_d_red_en;
-        d_reg_sel <= tem_d_reg_sel;
-
+        num_data <= tem_num_data;
+        parity <= tem_parity;
+        stop_2 <= tem_stop_2;
+        valid <= tem_valid;
+        
         @(posedge clk);
       end
-
+  
       forever begin // in monitor
         @ (posedge clk);
         data_mon_mbx.put(data);
-        d_reg_en_mon_mbx.put(d_reg_en);
-        d_reg_sel_mon_mbx.put(d_reg_sel);
+        num_data_mon_mbx.put(num_data);
+        parity_mon_mbx.put(parity);
+        stop_2_mon_mbx.put(stop_2);
+        valid_mon_mbx.put(valid);
       end
 
       forever begin // out monitor
         @ (posedge clk);
-        data_out_mon_mbx.put(data_out);
-
+        ready_mon_mbx.put(ready);
+        Tx_mon_mbx.put(Tx);
       end
-
-      forever begin // Scoreboard start
-        logic [7:0] expected_data;
-        logic expected_d_reg_en;
-        logic [2:0] expected_d_reg_sel;
-        logic expected_data_out;
-
-        logic dut_data_out; 
+      
+      forever begin // Scoreboard
+        logic expected_ready;
+        logic expected_Tx;
         
-        data_mon_mbx.get(expected_data);
-        d_reg_en_mon_mbx.get(expected_d_reg_en);
-        d_reg_sel_mon_mbx.get(expected_d_reg_sel);
-        
-        data_out_mon_mbx.get(dut_data_out);
+        ready_mon_mbx.get(expected_ready);
+        Tx_mon_mbx.get(expected_Tx);
 
-        if (expected_d_reg_en) begin
-          if (expected_d_reg_sel == 3'b000) expected_data_out = expected_data[0];
-          else if (expected_d_reg_sel == 3'b001) expected_data_out = expected_data[1];
-          else if (expected_d_reg_sel == 3'b010) expected_data_out = expected_data[2];
-          else if (expected_d_reg_sel == 3'b011) expected_data_out = expected_data[3];
-          else if (expected_d_reg_sel == 3'b100) expected_data_out = expected_data[4];
-          else if (expected_d_reg_sel == 3'b101) expected_data_out = expected_data[5];
-          else if (expected_d_reg_sel == 3'b110) expected_data_out = expected_data[6];
-          else if (expected_d_reg_sel == 3'b111) expected_data_out = expected_data[7];
-          else expected_data_out = '0;
+        // Implement your expected behavior comparison here.
+        // This is an example comparison, modify it according to your expected results
+        if (expected_ready == ready && expected_Tx == Tx) begin
+          pass++;
         end else begin
-          expected_data_out = '0;
+          fail++;
         end
-
-        if (expected_data_out == dut_data_out) pass++;
-        else fail++;
       end
     join_none
-
   endtask
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -152,21 +153,23 @@ module reg_piso_tb;
     $dumpfile("dump.vcd");
     $dumpvars;
     // Start clock
-    start_clk_i();
+    start_clk();
     // Apply reset
     apply_reset();
     // Start all the verification components
     driver_monitor_scoreboard();
-    // generate random data inputs
+    // Generate random data inputs
     @(posedge clk);
     repeat (100) begin
       data_dvr_mbx.put($urandom);
-      d_reg_en_dvr_mbx.put($urandom);
-      d_reg_sel_dvr_mbx.put($urandom);
+      num_data_dvr_mbx.put($urandom % 4);
+      parity_dvr_mbx.put($urandom % 2);
+      stop_2_dvr_mbx.put($urandom % 2);
+      valid_dvr_mbx.put($urandom % 2);
     end
     // Delay
     repeat (150) @(posedge clk);
-    // print results
+    // Print results
     $display("%0d/%0d PASSED", pass, pass+fail);
     // End simulation
     $finish;
